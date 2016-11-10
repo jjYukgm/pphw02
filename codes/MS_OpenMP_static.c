e4121c751d8fef8d656ae463015152a253500e78
jjYukgm <andykgm39@gmail.com>
2016-11-10 16:06:38 +0800

    calculation part -- i private?

--- codes/MS_OpenMP_static.c
+++ codes/MS_OpenMP_static.c
@@ -11,8 +11,8 @@
 #include <stdlib.h>
 //enable & disable
 #include <string.h>
-//#include <time.h>//time measure
-//#include <math.h>//time calculate
+#include <time.h>//time measure
+#include <math.h>//time calculate
 
 typedef struct complextype
 {
@@ -39,10 +39,10 @@ int main(int argc, char *argv[])
 
 		screen = DefaultScreen(display);
 	}
-	/*
+	
 	//time measure
 	struct timespec tt1, tt2;
-	clock_gettime(CLOCK_REALTIME, &tt1);*/
+	clock_gettime(CLOCK_REALTIME, &tt1);
 	
 	int thread_num = atoi(argv[1]);
 	double roffset  = atof(argv[2]);
@@ -84,15 +84,17 @@ int main(int argc, char *argv[])
 	
 	/* draw points */
 	int i, j;
-	#pragma omp parallel shared(window, gc , rscale, roffset, iscale, ioffset, i) private(  j ) num_threads(thread_num) 
+	#pragma omp parallel shared(window, gc , rscale, roffset, iscale, ioffset) private( i, j, tt2 ) num_threads(thread_num) 
 	{
+		int pt=0;
 		Compl z, c;
 		int repeats;
 		double temp, lengthsq;
 		//#pragma omp for schedule(static) nowait
-		#pragma omp for schedule(static)
+		#pragma omp for schedule(static) collapse(2)
 		for(i=0; i<width; i++) { 
 			for(j=0; j<height; j++) {
+				pt+=1;
 				z.real = 0.0;
 				z.imag = 0.0;
 				c.real = (double)i / rscale + roffset; /* Theorem : If c belongs to M(Mandelbrot set), then |c| <= 2 */
@@ -118,13 +120,14 @@ int main(int argc, char *argv[])
 				
 			}
 		}
+		clock_gettime(CLOCK_REALTIME, &tt2);
+		printf("[t%d]  pt: %d	;comp Time: %.3f sec\n", omp_get_thread_num(), pt, tt2.tv_sec - tt1.tv_sec+ tt2.tv_nsec*pow (10.0, -9.0) - tt1.tv_nsec*pow (10.0, -9.0));
 	}
 	
     if(able ==0){
 		XFlush(display);
 		sleep(5);
 	}/*
-	clock_gettime(CLOCK_REALTIME, &tt2);
 	printf("total time: %.3f sec\n ", tt2.tv_sec - tt1.tv_sec+ tt2.tv_nsec*pow (10.0, -9.0) - tt1.tv_nsec*pow (10.0, -9.0));*/
 	return 0;
 }
